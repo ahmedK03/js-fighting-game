087d03b9d37e2897f8945083d2ac692b3dcada9a
ahmedK03 <ahmed.khaled.fawzy@hotmail.com>
2022-04-18 06:02:26 +0200

    project setup

---
+++ main.js
@@ -0,0 +1,11 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 576;
+
+// add white background to the canvas
+
+c.fillRect( 0, 0, canvas.width, canvas.height);
+
+
